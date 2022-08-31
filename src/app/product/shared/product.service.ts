da2c338a4c6b95691a945dc44700c47b5d0076fe
miyu <mmmic1048@gmail.com>
2022-09-01 06:49:44 +0900

    Authフロントエンド

--- src/app/product/shared/product.service.ts
+++ src/app/product/shared/product.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { products } from 'src/app/products';
 
 @Injectable()
 export class ProductService {
