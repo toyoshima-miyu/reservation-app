6103a13285eb7cd89b6cc62aeb7076cbd994724b
miyu <mmmic1048@gmail.com>
2022-09-03 16:14:05 +0900

    fotfix

--- server/model/product.js
+++ server/model/product.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
-// const ObjectId = Schema.ObjectId;
  
 const ProductSchema = new Schema({
-  // author: ObjectId,
   coverImage: String,
   name: { type: String, required: true, max:[60, '最大60文字までです'] },
   price: Number,
