16dcb62abd8f2fa0bc1d265fb3285325fd57c4c7
miyu <mmmic1048@gmail.com>
2022-09-03 16:07:14 +0900

    認証ガード

---
+++ src/app/auth/shared/auth.guard.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { AuthService } from '../shared/auth.service';
+import { Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+
+
+@Injectable()
+export class AuthGuard {
+
+  constructor( private authService : AuthService, private router : Router ) {
+  }
+
+  canActivate( route : ActivatedRouteSnapshot, state : RouterStateSnapshot ) {
+    if(this.authService.isAuthenticated()) return true
+
+    this.router.navigate(['/login'])
+    return false
+  }
+}
